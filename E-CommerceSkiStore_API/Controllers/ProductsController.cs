d96a2755ff91106c17d3bac2643bb6d81cfc1e72
Haozhe Hu <Haozhe.Hu@student.uts.edu.au>
2024-12-02 17:16:47 +1100

    添加项目文件。

---
+++ E-CommerceSkiStore_API/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using E_CommerceSkiStore_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using E_CommerceSkiStore_API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceSkiStore_API.Controllers
+{
+    [ApiController]
+    [Route("E-CommerceSkiStore_API/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task <ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+            
+        }
+        [HttpGet("{id}")]
+        public async Task <ActionResult<Product>> GetProduct(int id)
+        { 
+            return await _context.Products.FindAsync(id);      
+        }
+    }
+}
---
+++ E-CommerceSkiStore_API/Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using E_CommerceSkiStore_API.Entities;
+
+namespace E_CommerceSkiStore_API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public DbSet<Product> Products{  get; set; }
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        
+    }
+}
