9c48672dc7dc0bded3bb1d91a1697a19c14e6ed8
Haozhe Hu <Haozhe.Hu@student.uts.edu.au>
2024-12-21 05:15:50 +1100

    add loadingbutton

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -1,5 +1,8 @@
+import { LoadingButton } from "@mui/lab";
 import { Avatar, Button, Card, CardActions, CardContent, CardHeader, CardMedia, Typography } from "@mui/material";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import agent from "../../app/api/agent";
 import { Product } from "../../app/models/products";
 
 
@@ -10,6 +13,17 @@ interface Props {
 
 
 export default function ProductCard({ product }: Props) {
+  const [loading, setLoading] = useState(false)
+
+  function handleAddItem(productId: number) {
+    setLoading(true)
+    agent.Basket.addItem(productId)
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+  }
+
+
+
   return (
     <Card >
       <CardHeader
@@ -37,7 +51,10 @@ export default function ProductCard({ product }: Props) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">Add to cart</Button>
+        <LoadingButton
+          loading={loading}
+          onClick={() => handleAddItem(product.id)}
+          size="small">Add to cart</LoadingButton>
         <Button component={Link} to={`/catalog/${product.id}`} size="small">View</Button>
       </CardActions>
     </Card>
