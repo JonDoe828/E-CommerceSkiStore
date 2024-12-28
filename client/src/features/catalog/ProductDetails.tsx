befe84b5f61b873c81cca52e86baa57523b8a70c
Haozhe Hu <Haozhe.Hu@student.uts.edu.au>
2024-12-29 04:30:04 +1100

    updating the product detail into the basket

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,20 +1,31 @@
-import { Divider, Table, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { LoadingButton } from '@mui/lab';
+import { Divider, Table, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material';
 import Grid from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import agent from '../../app/api/agent';
+import { useStoreContext } from '../../app/context/StoreContext';
 import NotFound from '../../app/errors/NotFound';
 import LoadingComponent from '../../app/layout/LoadingComponent';
 import { Product } from "../../app/models/products";
 
 export default function ProductDetails() {
+  const { basket } = useStoreContext()
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null)
   const [loading, setLoading] = useState(true)
-  useEffect(() => {
-    if (!id) return; // 如果 id 不存在，则退出
 
+  const [quantity, setQuantity] = useState(0)
+  const [submitting, setSubmitting] = useState(false)
+
+  const item = basket?.items.find(i => i.productId === product?.id)
+
+
+  useEffect(() => {
+    if (item) setQuantity(item.quantity)
+
+    if (!id) return; // 如果 id 不存在，则退出
     // 定义异步函数
     const fetchProduct = async () => {
       setLoading(true); // 显式设置加载状态
@@ -27,14 +38,12 @@ export default function ProductDetails() {
         setLoading(false); // 确保加载状态被正确清理
       }
     };
-
     fetchProduct();
-
     // 可选：清理函数
     return () => {
       setProduct(null); // 在组件卸载时清除状态
     };
-  }, [id]);
+  }, [id, item]);
 
   if (loading) return <LoadingComponent message='Loading Product...' />
   if (!product) return <NotFound />
@@ -73,6 +82,27 @@ export default function ProductDetails() {
             </TableHead>
           </Table>
         </TableContainer>
+        <Grid container spacing={2}>
+          <Grid size={{ xs: 6 }}>
+            <TextField
+              variant='outlined'
+              type='number'
+              label="Quantity in Cart"
+              fullWidth
+              value={quantity}
+            />
+          </Grid>
+          <Grid size={{ xs: 6 }}>
+            <LoadingButton
+              sx={{ height: '55px' }}
+              color='primary'
+              size='large'
+              variant='contained'
+              fullWidth>
+              {item ? 'Update Quantity' : 'Add to Cart'}
+            </LoadingButton>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   )
